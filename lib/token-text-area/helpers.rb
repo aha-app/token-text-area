5e780c250346fa0eaca25887f0184145b8fc843a
Chris Keele <dev@chriskeele.com>
2016-07-21 14:59:22 -0700

    Force raw html string to be html_safe.

--- lib/token-text-area/helpers.rb
+++ lib/token-text-area/helpers.rb
@@ -1,6 +1,6 @@
 module TokenTextArea
   module TokenTextAreaHelpers
-    
+
     def token_text_area(equation, variables, options = {})
       options.symbolize_keys!
 
@@ -12,21 +12,21 @@ module TokenTextArea
         class: "token-text-area noborder #{html_options[:class]}",
         data: data_options
       )
-      
+
       container_tag = options[:container_tag] || :div
-      
+
       content_tag(container_tag, tag_options) do
         content_tag(:div, class: 'token-text-area-input') do
           if equation.nil?
             ''
           else
-            equation.gsub!(/#[0-9]+#/) do 
+            equation.gsub!(/#[0-9]+#/) do
               cur_match = Regexp.last_match.to_s
               variable = variables.detect{ |v| v[:id] == cur_match.gsub('#','').to_i }
               if variable
                 content_tag(:span, class: 'token', data: { id: variable[:id] }) do
                   label = variable[:name]
-                  label += "&nbsp;<b>#{variable[:value].to_s}</b>" if variable[:value]
+                  label += "&nbsp;<b>#{variable[:value].to_s}</b>".html_safe if variable[:value]
                   label.html_safe
                 end
               else
